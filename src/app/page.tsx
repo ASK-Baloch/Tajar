df45c968fe20afa0bb73e6540cdfd08e2a0cf9e8
ASK-Baloch <balochsaeed888@gmail.com>
2023-12-28 17:13:41 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,34 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <MaxWidthWrapper>
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl ">
+            Your marketplace for high-quality{" "}
+            <span className="text-blue-600">Everything</span>.
+          </h1>
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+            Welcome to A1Watches. Every product on our Platform is build by our
+            own factory to ensure highest quality standards
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
+            <Link href="/products" className={buttonVariants()}>
+              Browse Trending
+            </Link>
+            <Button variant="ghost">Our Quality Promise &rarr;</Button>
+          </div>
+        </div>
+        {/* TODO : LIST PRODUCTS  */}
+      </MaxWidthWrapper>
+      <section>
+        <MaxWidthWrapper className="py-20">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0"></div>
+        </MaxWidthWrapper>
+      </section>
+    </>
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="z-10">
+      <div className="">
+        <ul className="flex gap-3 justify-center uppercase">
+          <li>Home</li>
+          <li>shop</li>
+          <li>watch</li>
+          <li>models</li>
+          <li>brands</li>
+          <li>material</li>
+          <li>pages</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
