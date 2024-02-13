b7001a46a11cd600848c6f7085ac959d8479f18c
ASK-Baloch <balochsaeed888@gmail.com>
2024-02-13 11:08:38 +0500

    server up and admin dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,11 +29,11 @@ export default function Home() {
         <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl ">
             Your marketplace for high-quality{" "}
-            <span className="text-blue-600">Watches</span>.
+            <span className="text-blue-600">Everything</span>.
           </h1>
           <p className="mt-6 text-lg max-w-prose text-muted-foreground">
-            Welcome to A1Watches. Every product on our Platform is build by our
-            own factory to ensure highest quality standards
+            Welcome to Tajars. Every product on our Platform is build by our own
+            factory to ensure highest quality standards
           </p>
           <div className="flex flex-col sm:flex-row gap-4 mt-6">
             <Link href="/products" className={buttonVariants()}>
---
+++ src/payload.config.ts
@@ -0,0 +1,43 @@
+import { buildConfig } from "payload/config";
+import { mongooseAdapter} from "@payloadcms/db-mongodb";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+import path from "path";
+
+
+export default buildConfig({
+    serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
+    collections: [],
+    routes:{
+        admin: '/sell'
+      },
+      admin:{
+        bundler:webpackBundler(),
+        meta:{
+            titleSuffix:" - Tajar",
+            favicon:"/tajar_icon.ico",
+            ogImage:"/tajar.png",
+        }
+      },
+      rateLimit:{
+        max:2000,
+      },
+      editor: slateEditor({}),
+      db:mongooseAdapter({
+        url:process.env.MONGO_URL!,
+      }),
+      typescript:{
+        outputFile: path.resolve(__dirname, "payload-types.ts"),
+      },
+    }
+    // plugins: [],
+    // hooks: {},
+    // upload: {},
+    // locales: [],
+    // i18n: {},
+    // navigation: [],
+    // customRoutes: [],
+    // customViews: [],
+    // customActions
+
+)
