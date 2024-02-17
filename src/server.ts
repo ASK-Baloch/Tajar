f9ccd88e1b180a55f71a35c7007b332231d723d8
ASK-Baloch <balochsaeed888@gmail.com>
2024-02-17 10:16:09 +0500

    authentication done and navbar

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,46 @@
-import express from 'express';
-import { getPayloadClient } from './get-playload';
-import { nextApp, nextHandler } from './next-utils';
+import express from "express";
+import { getPayloadClient } from "./get-playload";
+import { nextApp, nextHandler } from "./next-utils";
+import { appRouter } from "./trpc";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { inferAsyncReturnType } from "@trpc/server";
 
 const app = express();
-const PORT = Number(process.env.PORT) || 3000
+const PORT = Number(process.env.PORT) || 3000;
+
+const createContext =
+  () =>
+  ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({
+    req,
+    res,
+  });
+
+export type ExpressContext = inferAsyncReturnType<typeof createContext>;
 
 const start = async () => {
-     const payload = await getPayloadClient({initOptions:{express:app,onInit:async(cms)=>{
-        cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
-     }}})
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
+      },
+    },
+  });
 
-     app.use((req,res) => nextHandler(req,res))
-     nextApp.prepare().then(() => {
-    //    payload.logger.info("Next.js Started")
-       app.listen(PORT,async () => {
-        // payload.logger.info(`Nextjs App URL:${process.env.NEXT_PUBLIC_SERVER_URL}`)
-       })
-     })
-}
+  app.use(
+    "/api/trpc",
+    // @ts-expect-error context already passed from express middleware
+    trpcExpress.createExpressMiddleware({ router: appRouter, createContext})
+  );
+
+  app.use((req, res) => nextHandler(req, res));
+  nextApp.prepare().then(() => {
+       payload.logger.info("Next.js Started")
+    app.listen(PORT, async () => {
+      payload.logger.info(`Nextjs App URL:${process.env.NEXT_PUBLIC_SERVER_URL}`)
+    });
+  });
+};
 
 start();
 
