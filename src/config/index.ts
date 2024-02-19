31cdbeedd5a601ba074f2f145de1ed2d7bcff025
ASK-Baloch <balochsaeed888@gmail.com>
2024-02-19 20:12:37 +0500

    product slider ongoing .working on UI

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,44 +1,44 @@
 export const PRODUCT_CATEGORIES = [
-    {
-        label:"WATCHES",
-        value:"watch-kits" as const,
-        featured: [
-            {
-                name:"Editor Picks",
-                href:"#",
-                imageSrc:"/nav/watch-kits/rolex.webp"
-            },
-            {
-                name:"New Arrival",
-                href:"#",
-                imageSrc:"/nav/watch-kits/apple.jpg"
-            },
-            {
-                name:"Bestsellers",
-                href:"#",
-                imageSrc:"/nav/watch-kits/curren.webp"
-            },
-        ]
-    },
-    {
-        label:"Art",
-        value:"icons" as const,
-        featured: [
-            {
-                name:"Favourite Icon Picks",
-                href:"#",
-                imageSrc:"/nav/icons/picks.png"
-            },
-            {
-                name:"New Arrival",
-                href:"#",
-                imageSrc:"/nav/icons/new.webp"
-            },
-            {
-                name:"Bestsellers",
-                href:"#",
-                imageSrc:"/nav/icons/bestsellers.webp"
-            },
-        ]
-    }
-]
+  {
+    label: "Wallet",
+    value: "icons",
+    featured: [
+      {
+        name: "Leather Wallets",
+        href: "#",
+        imageSrc: "/nav/icons/picks.png",
+      },
+      {
+        name: "New inforced Wallet",
+        href: "#",
+        imageSrc: "/nav/icons/new.webp",
+      },
+      {
+        name: "Best Original Wallet",
+        href: "#",
+        imageSrc: "/nav/icons/bestsellers.webp",
+      },
+    ],
+  },
+  {
+    label: "Hats & Caps",
+    value: "icons",
+    featured: [
+      {
+        name: "simple & Classic High Quality",
+        href: "#",
+        imageSrc: "/nav/icons/picks.png",
+      },
+      {
+        name: "Hats on Order ",
+        href: "#",
+        imageSrc: "/nav/icons/new.webp",
+      },
+      {
+        name: "Variety of colours",
+        href: "#",
+        imageSrc: "/nav/icons/bestsellers.webp",
+      },
+    ],
+  },
+];
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,5 @@
 import { buildConfig } from "payload/config";
-import { mongooseAdapter} from "@payloadcms/db-mongodb";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import path from "path";
@@ -12,42 +12,31 @@ import { Orders } from "./collections/Orders";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
-})
+});
 
 export default buildConfig({
-    serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-    collections: [Users , Products , Media , ProductFiles , Orders],
-    routes:{
-        admin: '/sell'
-      },
-      admin:{
-        user:"users",
-        bundler:webpackBundler(),
-        meta:{
-            titleSuffix:" - Tajar",
-            favicon:"/tajar_icon.ico",
-            ogImage:"/tajar.png",
-        }
-      },
-      rateLimit:{
-        max:2000,
-      },
-      editor: slateEditor({}),
-      db:mongooseAdapter({
-        url:process.env.MONGO_URL!,
-      }),
-      typescript:{
-        outputFile: path.resolve(__dirname, "payload-types.ts"),
-      },
-    }
-    // plugins: [],
-    // hooks: {},
-    // upload: {},
-    // locales: [],
-    // i18n: {},
-    // navigation: [],
-    // customRoutes: [],
-    // customViews: [],
-    // customActions
-
-)
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
+  collections: [Users, Products, Media, ProductFiles, Orders],
+  routes: {
+    admin: "/sell",
+  },
+  admin: {
+    user: "users",
+    bundler: webpackBundler(),
+    meta: {
+      titleSuffix: " - Tajar",
+      favicon: "/tajar_icon.ico",
+      ogImage: "/tajar.png",
+    },
+  },
+  rateLimit: {
+    max: 2000,
+  },
+  editor: slateEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGO_URL!,
+  }),
+  typescript: {
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
+  },
+});
