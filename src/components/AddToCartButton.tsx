f922ff34b78ab0e4e78b3fa50bb35d49ed121bf4
ASK-Baloch <balochsaeed888@gmail.com>
2024-02-25 14:50:11 +0500

    product desc page and cart done

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -2,18 +2,30 @@
 
 import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
+import { useCart } from "@/hooks/use-cart";
+import { Product } from "@/payload-types";
 
-const AddToCartButton = () => {
-  const [isSuccess, setIsSuccess] = useState<Boolean>(false);
+const AddToCartButton = ({ product }: { product: Product }) => {
+  const { addItem } = useCart();
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsSuccess(false);
     }, 2000);
+
     return () => clearTimeout(timeout);
   }, [isSuccess]);
+
   return (
-    <Button onClick={() => setIsSuccess(true)} size="lg" className="w-full">
+    <Button
+      onClick={() => {
+        addItem(product);
+        setIsSuccess(true);
+      }}
+      size="lg"
+      className="w-full"
+    >
       {isSuccess ? "Added!" : "Add to cart"}
     </Button>
   );
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import { ShoppingCart } from "lucide-react";
 import {
   Sheet,
   SheetContent,
@@ -7,53 +9,79 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "./ui/sheet";
-import { ShoppingCart } from "lucide-react";
 import { Separator } from "./ui/separator";
 import { formatPrice } from "@/lib/utils";
-import { buttonVariants } from "./ui/button";
 import Link from "next/link";
+import { buttonVariants } from "./ui/button";
 import Image from "next/image";
+import { useCart } from "@/hooks/use-cart";
+import { ScrollArea } from "./ui/scroll-area";
+import CartItem from "./CartItem";
+import { useEffect, useState } from "react";
 
 const Cart = () => {
-  const itemCount = 0;
-  const fee = 150;
+  const { items } = useCart();
+  const itemCount = items.length;
+
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  const cartTotal = items.reduce((total, { product }) => {
+    if (product && typeof product.price === "number") {
+      return total + product.price;
+    }
+    return total;
+  }, 0);
+
+  const fee = 1;
+
   return (
     <Sheet>
-      <SheetTrigger className="group -m-2 flex items-center ">
+      <SheetTrigger className="group -m-2 flex items-center p-2">
         <ShoppingCart
           aria-hidden="true"
           className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
         />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-          0
+          {isMounted ? itemCount : 0}
         </span>
       </SheetTrigger>
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
         <SheetHeader className="space-y-2.5 pr-6">
-          <SheetTitle>Cart (0)</SheetTitle>
+          <SheetTitle>Cart ({itemCount})</SheetTitle>
         </SheetHeader>
         {itemCount > 0 ? (
           <>
             <div className="flex w-full flex-col pr-6">
-              {/* TODO: CART LOGIC */}
-              cart items
+              <ScrollArea>
+                {items.map(
+                  ({ product }) =>
+                    product &&
+                    product.id && (
+                      <CartItem product={product} key={product.id} />
+                    )
+                )}
+              </ScrollArea>
             </div>
             <div className="space-y-4 pr-6">
               <Separator />
-            </div>
-            <div className="space-y-1.5 pr-6">
-              <div className="flex">
-                <span className="flex-1">Shipping</span>
-                <span>Free</span>
+              <div className="space-y-1.5 text-sm">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Fee</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Total</span>
+                  <span>{formatPrice(cartTotal + fee)}</span>
+                </div>
               </div>
-              <div className="flex">
-                <span className="flex-1">Transaction Fee</span>
-                <span>{formatPrice(fee)}</span>
-              </div>
-              <div className="flex">
-                <span className="flex-1">Total</span>
-                <span>{formatPrice(fee)}</span>
-              </div>
+
               <SheetFooter>
                 <SheetTrigger asChild>
                   <Link
@@ -76,25 +104,23 @@ const Cart = () => {
             >
               <Image
                 src="/noproduct-cart.jpg"
-                alt="empty shopping cart product "
-                className="border-none rounded w-full"
                 fill
+                alt="empty tajar-cart image"
               />
             </div>
-            <div className="text-xl font-semibold">
-              <SheetTrigger asChild>
-                <Link
-                  href="/products"
-                  className={buttonVariants({
-                    variant: "link",
-                    size: "sm",
-                    className: "text-sm text-muted-foreground",
-                  })}
-                >
-                  Add items to your Cart to Checkout
-                </Link>
-              </SheetTrigger>
-            </div>
+            <div className="text-xl font-semibold">Your cart is empty</div>
+            <SheetTrigger asChild>
+              <Link
+                href="/products"
+                className={buttonVariants({
+                  variant: "link",
+                  size: "sm",
+                  className: "text-sm text-muted-foreground",
+                })}
+              >
+                Add items to your cart to checkout
+              </Link>
+            </SheetTrigger>
           </div>
         )}
       </SheetContent>
