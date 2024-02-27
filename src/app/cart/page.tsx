9445da27ccc35b676a8d2e75f550c80444ac7d4c
ASK-Baloch <balochsaeed888@gmail.com>
2024-02-27 19:52:14 +0500

    cart page working started

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
+        good things take time
+      </div>
+    </div>
+  );
+};
+export default Page;
