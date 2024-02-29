09a43a8f836cdef3efe9429398fa9353d1969c02
ASK-Baloch <balochsaeed888@gmail.com>
2024-02-29 13:58:43 +0500

    cart page doe . creating checkout payment session

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,50 +3,59 @@ import { authRouter } from "./auth-router";
 import { router, publicProcedure } from "./trpc";
 import { QueryValidator } from "../lib/validators/query-validator";
 import { getPayloadClient } from "../get-playload";
+import { paymentRouter } from "./payment-router";
 
 export const appRouter = router({
-    auth: authRouter,
+  auth: authRouter,
+  payment: paymentRouter,
 
-    getInfiniteProducts:publicProcedure.input(z.object({
-        limit:z.number().min(1).max(100),
-        cursor:z.number().nullish(),
-        query:QueryValidator
-    })).query( async ({input}) => {
-        const {query , cursor} = input
-        const {sort , limit , ...queryOpts} = query
+  getInfiniteProducts: publicProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100),
+        cursor: z.number().nullish(),
+        query: QueryValidator,
+      })
+    )
+    .query(async ({ input }) => {
+      const { query, cursor } = input;
+      const { sort, limit, ...queryOpts } = query;
 
-        const payload = await getPayloadClient();
+      const payload = await getPayloadClient();
 
-        const parsedQueryOpts:Record<string,{equals:string}> = {}
+      const parsedQueryOpts: Record<string, { equals: string }> = {};
 
-        Object.entries(queryOpts).forEach(([key,value]) => {
-            parsedQueryOpts[key] = {
-                equals:value
-            }
-        })
-        
-        const page = cursor || 1
+      Object.entries(queryOpts).forEach(([key, value]) => {
+        parsedQueryOpts[key] = {
+          equals: value,
+        };
+      });
 
-        const {docs:items , hasNextPage , nextPage} = await payload.find({
-            collection:"products",
-            where:{
-                approvedForSale:{
-                    equals:'approved'
-                },
-                ...parsedQueryOpts
-            },
-            sort,
-            depth:1,
-            limit,
-            page,
-        })
+      const page = cursor || 1;
 
-        return {
-            items,
-            nextPage: hasNextPage ? nextPage : null,
-        }
-        
-    })
+      const {
+        docs: items,
+        hasNextPage,
+        nextPage,
+      } = await payload.find({
+        collection: "products",
+        where: {
+          approvedForSale: {
+            equals: "approved",
+          },
+          ...parsedQueryOpts,
+        },
+        sort,
+        depth: 1,
+        limit,
+        page,
+      });
+
+      return {
+        items,
+        nextPage: hasNextPage ? nextPage : null,
+      };
+    }),
 });
 
-export type AppRouter = typeof appRouter; 
+export type AppRouter = typeof appRouter;
