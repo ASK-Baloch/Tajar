a5d5b83a1ddb6d5267d3dee27858f66d755f6d0a
Ahmed <balochsaeed888@gmail.com>
2024-03-02 15:44:07 +0500

    all flow done . just styling remaining

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,6 +1,42 @@
+import { getServersideUser } from "@/lib/payload-utils";
 import Image from "next/image";
+import { cookies } from "next/headers";
+import next from "next";
+import { getPayloadClient } from "../../get-playload";
+import { notFound, redirect } from "next/navigation";
+import { Product, ProductFile } from "../../payload-types";
+import { PRODUCT_CATEGORIES } from "../../config";
 
-const ThankYouPage = () => {
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const ThankYouPage = async ({ searchParams }: PageProps) => {
+  const orderId = searchParams.orderId;
+  const nextCookies = cookies();
+  const { user } = await getServersideUser(nextCookies);
+  const payload = await getPayloadClient();
+
+  const { docs: orders } = await payload.find({
+    collection: "orders",
+    depth: 2,
+    where: {
+      id: { equals: orderId },
+    },
+  });
+
+  const [order] = orders;
+  if (!order) return notFound();
+
+  const orderUserId =
+    typeof order.user === "string" ? order.user : order.user.id;
+
+  if (orderUserId !== user?.id) {
+    return redirect(`/sign-in?origin=/thank-you?orderId=${order.id}`);
+  }
+
   return (
     <main className="relative lg:min-h-full">
       <div className="hidden md:block h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
@@ -13,9 +49,52 @@ const ThankYouPage = () => {
       </div>
       <div>
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32 xl:gap-x-24">
-            <div className="lg:col-start-2 ">
-                <h1 className="text-sm font-medium text-blue-600">Order Successful</h1>
-            </div>
+          <div className="lg:col-start-2 ">
+            <p className="text-sm font-medium text-blue-600">
+              Order Successful
+            </p>
+            <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+              Thanks for Ordering
+            </h1>
+            {order._isPaid ? <p className="mt-2 text-base text-muted-foreground">
+              {/* We&apos;ll email you when your order is on its way! */}
+              {/* Your order is on its way! */}
+              your order has been placed successfully and is on its way.We&apos;ll email  the details and receipt to {" "} {typeof order.user !== "string" ? (<span className="font-medium text-gray-900">{order.user.email}</span>) : null }.
+            </p> : <p className="mt-2 text-base text-muted-foreground">
+              we appreciate your order.We&apos;re currently Processing it. So hang tight and  We&apos;ll email you for confirmation when your order is Placed</p>}
+
+              <div className="mt-16 text-sm font-medium">
+                <div className="text-muted-foreground">Order No.</div>
+                <div className="mt-2 text-gray-900">{order.id}</div>
+                <ul className="mt-6 divide-y divide-gray-200 border-t border-gray-200 text-sm font-medium text-muted-foreground">
+                  {(order.products as Product[]).map((product) => {
+                        const label = PRODUCT_CATEGORIES.find(
+                          ({ value }) => value === product.category
+                        )?.label;
+                      const downloadUrl = (product.product_files as ProductFile).url as string;  
+                      const {image} = product.images[0]
+                    return <li key={product.id} className="flex space-x-6 py-6">
+                      <div className="relative h-24 w-24 ">
+                        {typeof image !== 'string' && image.url ?  (<Image src={image.url} fill alt={`${product.name} Image `} className="flex-none rounded-md bg-gray-100 object-cover object-center " />) : null}
+                      </div>
+                      <div className="flex flex-auto flex-col justify-between">
+                        <div className="space-y-1">
+                          <h3 className="text-gray-900">{product.name}</h3>
+                          <p className="my-1">Category : {label}</p>
+                        </div>
+                        {order._isPaid ? (
+                          <a href={downloadUrl} download={product.name} 
+                          className="text-blue-600 hover:underline underline-offset-2 ">
+                            Download  Asset
+                          </a>
+                        ) : null}
+                      </div>
+                    </li>
+                  })}
+                </ul>
+              </div>
+
+          </div>
         </div>
       </div>
     </main>
