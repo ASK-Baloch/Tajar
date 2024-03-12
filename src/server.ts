5be0cf61df5cf2acc1287dfb404215e347b2decd
Ahmed <balochsaeed888@gmail.com>
2024-03-12 20:07:29 +0500

    making our own stripewebhook

--- src/server.ts
+++ src/server.ts
@@ -4,20 +4,29 @@ import { nextApp, nextHandler } from "./next-utils";
 import { appRouter } from "./trpc";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { inferAsyncReturnType } from "@trpc/server";
+import bodyParser from "body-parser";
+import { IncomingMessage } from "http";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
 const createContext =
   () =>
-  ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({
-    req,
-    res,
-  });
+    ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({
+      req,
+      res,
+    });
 
 export type ExpressContext = inferAsyncReturnType<typeof createContext>;
+export type WebhookRequest = IncomingMessage & { rawBody: Buffer };
 
 const start = async () => {
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer
+    },
+  });
+  app.post("/api/webhooks/stripe", webhookMiddleware,stripeWebhookHandler);
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
