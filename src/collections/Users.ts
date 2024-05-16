83fafd0a277e7afc04c3faa92dbe11d2cf9dff7d
Ahmed <balochsaeed888@gmail.com>
2024-05-16 19:05:15 +0500

    changed email from html to good looking email comonenet

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,19 +1,62 @@
-import { CollectionConfig } from "payload/types";
+import { PrimaryActionEmailHtml } from "@/components/emails/PrimaryActionEmail";
+import { Access, CollectionConfig } from "payload/types";
 
-export const Users: CollectionConfig = {
+const adminsAndUser: Access = ({ req: { user } }) => {
+  if (user.role === "admin") return true;
+
+  return {
+    id: {
+      equals: user.id
+    }
+  }
+}
+
+export const Users: CollectionConfig = { 
   slug: "users",
   auth: {
     verify: {
       generateEmailHTML: ({ token }) => {
-        return `<a href='${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}'>verify account</a>`;
+        return PrimaryActionEmailHtml({
+          actionLabel:"verify your email",
+          buttonText:"Verify account",
+          href:`${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`
+        })
       },
     },
   },
   access: {
-    read: () => true,
+    read: adminsAndUser,
     create: () => true,
+    update: ({req}) => req.user.role === "admin",
+    delete: ({req}) => req.user.role === "admin",
   },
+  admin:{
+    hidden:({user}) => user.role !== "admin",
+    defaultColumns:['id']
+  }
+  ,
   fields: [
+    {
+      name: "products",
+      label: "products",
+      admin: {
+        condition: () => false
+      },
+      type: "relationship",
+      relationTo: "products",
+      hasMany: true,
+    },
+    //               TO BE REPLACE  #########################################
+    {
+      name: "product_files",
+      label: "product_files",
+      admin: {
+        condition: () => false
+      },
+      type: "relationship",
+      relationTo: "product_files",
+      hasMany: true,
+    },
     {
       name: "role",
       defaultValue: "user",
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,16 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { getServersideUser } from "./lib/payload-utils";
 
-export async function middleware(req:NextRequest) {
-    
+export async function middleware(req: NextRequest) {
+    const { nextUrl, cookies } = req
+    const { user} = await getServersideUser(cookies)
+
+    if (
+        user &&
+        ['/sign-in', '/sign-up'].includes(nextUrl.pathname)
+    ) {
+        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/`)
+    }
+
+    return NextResponse.next()
 }
